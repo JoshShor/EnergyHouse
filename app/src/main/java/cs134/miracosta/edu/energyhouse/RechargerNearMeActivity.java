7d909d51b2249b4ae232824c3e243f1fb951fa26
bachavez <bachavez@yahoo.com>
2019-05-10 10:26:32 -0700

    Brian-Commit layouts

---
+++ app/src/main/java/cs134/miracosta/edu/energyhouse/RechargerNearMeActivity.java
@@ -0,0 +1,13 @@
+package cs134.miracosta.edu.energyhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RechargerNearMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recharger_near_me_activty);
+    }
+}
