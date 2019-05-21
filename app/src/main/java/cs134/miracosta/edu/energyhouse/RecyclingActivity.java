92b59a0764ab534df140523083dd4fd8322ecbfc
Dennis La <dennisla95@yahoo.com>
2019-05-21 14:12:38 -0700

    Update RecyclingActivity.java
    add javadocs

--- app/src/main/java/cs134/miracosta/edu/energyhouse/RecyclingActivity.java
+++ app/src/main/java/cs134/miracosta/edu/energyhouse/RecyclingActivity.java
@@ -8,6 +8,12 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+/**
+ * RecyclingActivity.java - Gives user a menu to navigate the Recycling activities
+ *
+ * @author Dennis La
+ * @version 1.0
+ */
 public class RecyclingActivity extends AppCompatActivity {
 
     private static Intent sIntent;
@@ -15,6 +21,11 @@ public class RecyclingActivity extends AppCompatActivity {
     private ImageView landscapeImageView;
     private Animation fadeInAnimation;
 
+    /**
+     * Creates the activity
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,24 +39,44 @@ public class RecyclingActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Goes to WhereToRecycleActivity
+     *
+     * @param v where to recycle button
+     */
     public void goToWhereToActivity(View v)
     {
         sIntent = new Intent(this, WhereToRecycleActivity.class);
         startActivity(sIntent);
     }
 
+    /**
+     * Goes to WhatGoesWhereActivity
+     *
+     * @param v what goes where button
+     */
     public void goToGoesWhereActivity(View v)
     {
         sIntent = new Intent(this, WhatGoesWhereActivity.class);
         startActivity(sIntent);
     }
 
+    /**
+     * Goes to CalculateRecyclablesActivity
+     *
+     * @param v calculate recyclables button
+     */
     public void goToCalcRecActivity(View v)
     {
         sIntent = new Intent(this, CalculateRecyclablesActivity.class);
         startActivity(sIntent);
     }
 
+    /**
+     * Goes to ImpactActivity
+     *
+     * @param v impact tracker button
+     */
     public void gotToImpactActivity(View v)
     {
         sIntent = new Intent(this, ImpactActivity.class);
