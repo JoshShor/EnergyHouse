be5210efb457c4021fcc30918b58f01b2e545bf6
Dennis La <dennisla95@yahoo.com>
2019-05-21 13:07:11 -0700

    Update RecyclingTypes.java
    add javadocs

--- app/src/main/java/cs134/miracosta/edu/energyhouse/model/RecyclingTypes.java
+++ app/src/main/java/cs134/miracosta/edu/energyhouse/model/RecyclingTypes.java
@@ -1,33 +1,68 @@
 package cs134.miracosta.edu.energyhouse.model;
 
+/**
+ * RecyclingTypes.java - Class to represent the different recyclable types
+ *
+ * @author Dennis La
+ * @version 1.0
+ */
 public class RecyclingTypes
 {
     private String mRecyclableName;
     private String mHowToDispose;
 
+    /**
+     * Default constructor
+     */
     public RecyclingTypes()
     {
         mRecyclableName = "Unknown";
         mHowToDispose = "Unknown";
     }
 
+    /**
+     * Full constructor that takes in both data members
+     *
+     * @param recyclableName the name of the recyclable
+     * @param howToDispose how to dispose the item
+     */
     public RecyclingTypes(String recyclableName, String howToDispose) {
         mRecyclableName = recyclableName;
         mHowToDispose = howToDispose;
     }
 
+    /**
+     * Get the name of the recyclable type
+     *
+     * @return the name of the recyclable type
+     */
     public String getRecyclableName() {
         return mRecyclableName;
     }
 
+    /**
+     * Set the name of the recyclable type
+     *
+     * @param recyclableName the name of the recyclable type
+     */
     public void setRecyclableName(String recyclableName) {
         mRecyclableName = recyclableName;
     }
 
+    /**
+     * Get the description of how to dispose of the recyclable type
+     *
+     * @return the description of how to dispose of the recyclable type
+     */
     public String getHowToDispose() {
         return mHowToDispose;
     }
 
+    /**
+     * Set the description of how to dispose of the recyclable type
+     *
+     * @param howToDispose the description of how to dispose of the recyclable type
+     */
     public void setHowToDispose(String howToDispose) {
         mHowToDispose = howToDispose;
     }
